14f4028dbea17da5a2c65ce16718103a344b0091
admin <admin@delimeat.io>
2016-01-16 15:35:59 +1100

    added test cases for ReplaceContentTypeResponseFilter

--- src/main/java/io/delimeat/util/jaxrs/ReplaceContentTypeResponseFilter.java
+++ src/main/java/io/delimeat/util/jaxrs/ReplaceContentTypeResponseFilter.java
@@ -1,18 +1,25 @@
 package io.delimeat.util.jaxrs;
 
+import io.delimeat.util.DelimeatUtils;
+
 import java.io.IOException;
 
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientResponseContext;
 import javax.ws.rs.client.ClientResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
 
 public class ReplaceContentTypeResponseFilter implements ClientResponseFilter {
 
 	@Override
 	public void filter(ClientRequestContext request, ClientResponseContext response)
 			throws IOException {
-		response.getHeaders().remove("Content-Type");
-		response.getHeaders().add("Content-Type", request.getHeaderString("Accept"));
+		final String acceptString = request.getHeaderString("Accept");
+		final MultivaluedMap<String, String> headers = response.getHeaders();
+		if(DelimeatUtils.isNotEmpty(acceptString) &&  headers.getFirst("Content-Type") != acceptString){
+			headers.remove("Content-Type");
+			headers.add("Content-Type", acceptString);
+		}
 	}
 
 }
---
+++ src/test/java/io/delimeat/util/jaxrs/ReplaceContentTypeResponseFilterTest.java
@@ -0,0 +1,78 @@
+package io.delimeat.util.jaxrs;
+
+import java.io.IOException;
+
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientResponseContext;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ReplaceContentTypeResponseFilterTest {
+
+	private ReplaceContentTypeResponseFilter filter; 
+	private MultivaluedMap<String, String> headers;
+	
+	@Before
+	public void setUp() throws Exception {
+		filter = new ReplaceContentTypeResponseFilter();
+		headers = new MultivaluedHashMap<String,String>();
+	}
+	
+	@Test
+	public void filterNullTest() throws IOException{
+		ClientRequestContext request = Mockito.mock(ClientRequestContext.class);
+		Mockito.when(request.getHeaderString("Accept")).thenReturn(null);
+		
+		headers.add("Content-Type", "CONTENT");
+		ClientResponseContext response = Mockito.mock(ClientResponseContext.class);
+		Mockito.when(response.getHeaders()).thenReturn(headers);
+		
+		filter.filter(request, response);
+		Assert.assertEquals("CONTENT", headers.getFirst("Content-Type"));		
+	}
+	
+	@Test
+	public void filterEmptyTest() throws IOException{
+		ClientRequestContext request = Mockito.mock(ClientRequestContext.class);
+		Mockito.when(request.getHeaderString("Accept")).thenReturn("");
+		
+		headers.add("Content-Type", "CONTENT");
+		ClientResponseContext response = Mockito.mock(ClientResponseContext.class);
+		Mockito.when(response.getHeaders()).thenReturn(headers);
+		
+		filter.filter(request, response);
+		Assert.assertEquals("CONTENT", headers.getFirst("Content-Type"));
+	}
+
+	@Test
+	public void filterNotMatchTest() throws IOException{
+		ClientRequestContext request = Mockito.mock(ClientRequestContext.class);
+		Mockito.when(request.getHeaderString("Accept")).thenReturn("CONTENT");
+		
+		headers.add("Content-Type", "DIFFERENT");
+		ClientResponseContext response = Mockito.mock(ClientResponseContext.class);
+		Mockito.when(response.getHeaders()).thenReturn(headers);
+		
+		filter.filter(request, response);
+		Assert.assertEquals("CONTENT", headers.getFirst("Content-Type"));
+	}
+	
+	@Test
+	public void filterMatchTest() throws IOException{
+		ClientRequestContext request = Mockito.mock(ClientRequestContext.class);
+		Mockito.when(request.getHeaderString("Accept")).thenReturn("CONTENT");
+		
+		headers.add("Content-Type", "CONTENT");
+		ClientResponseContext response = Mockito.mock(ClientResponseContext.class);
+		Mockito.when(response.getHeaders()).thenReturn(headers);
+		
+		filter.filter(request, response);
+		Assert.assertEquals("CONTENT", headers.getFirst("Content-Type"));
+	}
+
+}
