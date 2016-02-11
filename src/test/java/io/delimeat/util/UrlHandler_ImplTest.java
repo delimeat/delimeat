650e95d9c44a4381a9b300f09ff416fc32742934
Anonymous <admin@delimeat.io>
2016-02-11 02:19:44 +0000

    added test cases for GZIP/Delfate input streams from URLConnection in UrlHandler_Impl

--- src/test/java/io/delimeat/util/UrlHandler_ImplTest.java
+++ src/test/java/io/delimeat/util/UrlHandler_ImplTest.java
@@ -5,6 +5,13 @@ import io.delimeat.util.UrlHandler_Impl;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.InflaterInputStream;
 
 //TODO add additional tests
 public class UrlHandler_ImplTest {
@@ -22,4 +29,44 @@ public class UrlHandler_ImplTest {
 		handler.setTimeout(Integer.MAX_VALUE);
 		Assert.assertEquals(Integer.MAX_VALUE, handler.getTimeout());
 	}
+  
+  	public void openInputUrlConnectionGZIP() throws IOException{
+   	URLConnection connection = Mockito.mock(URLConnection.class);
+     	Mockito.when(connection.getContentEncoding()).thenReturn("gzip");
+     	InputStream input = Mockito.mock(InputStream.class);
+     	Mockito.when(connection.getInputStream()).thenReturn(input);
+     
+     	InputStream returnedInput = handler.openInput(connection);
+     	Assert.assertTrue(returnedInput instanceof GZIPInputStream); 
+   }
+  
+  	public void openInputUrlConnectionDeflate() throws IOException{
+   	URLConnection connection = Mockito.mock(URLConnection.class);
+     	Mockito.when(connection.getContentEncoding()).thenReturn("deflate");
+     	InputStream input = Mockito.mock(InputStream.class);
+     	Mockito.when(connection.getInputStream()).thenReturn(input);
+     
+     	InputStream returnedInput = handler.openInput(connection);
+     	Assert.assertTrue(returnedInput instanceof InflaterInputStream); 
+   }
+  
+  	public void openInputUrlConnectionNull() throws IOException{
+   	URLConnection connection = Mockito.mock(URLConnection.class);
+     	Mockito.when(connection.getContentEncoding()).thenReturn(null);
+     	InputStream input = Mockito.mock(InputStream.class);
+     	Mockito.when(connection.getInputStream()).thenReturn(input);
+     
+     	InputStream returnedInput = handler.openInput(connection);
+     	Assert.assertEquals(input, returnedInput); 
+   }
+  
+  	public void openInputUrlConnectionNotDeflateOrGZIP() throws IOException{
+   	URLConnection connection = Mockito.mock(URLConnection.class);
+     	Mockito.when(connection.getContentEncoding()).thenReturn("JIbberIsh");
+     	InputStream input = Mockito.mock(InputStream.class);
+     	Mockito.when(connection.getInputStream()).thenReturn(input);
+     
+     	InputStream returnedInput = handler.openInput(connection);
+     	Assert.assertEquals(input, returnedInput); 
+   }
 }
