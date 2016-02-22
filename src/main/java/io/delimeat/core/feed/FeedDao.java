3548835765db9753776a5a35cf3b81dd77a17b53
Anonymous <admin@delimeat.io>
2016-02-22 22:38:19 +0000

    fix for feed dao

--- src/main/java/io/delimeat/core/feed/FeedDao.java
+++ src/main/java/io/delimeat/core/feed/FeedDao.java
@@ -2,11 +2,10 @@ package io.delimeat.core.feed;
 
 import java.util.List;
 
-
 public interface FeedDao {
 
 	public FeedSource getFeedSource();
 	
-	public List<FeedResult> read(String showTitle) throws FeedException;
+	public List<FeedResult> read(String title) throws FeedException;
 	
 }
