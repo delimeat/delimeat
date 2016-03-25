f7feac7b9fba119532d21514151811b553226a8e
admin <admin@delimeat.io>
2016-03-25 11:33:12 +1100

    move to jndi datasource and use persistence.xml

--- src/main/java/io/delimeat/core/ApplicationStartupListener.java
+++ src/main/java/io/delimeat/core/ApplicationStartupListener.java
@@ -1,96 +1,29 @@
 package io.delimeat.core;
 
-import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
 @WebListener
 public class ApplicationStartupListener implements ServletContextListener {
 
-	// supported os types
-	private static final String WINDOWS = "windows";
-	private static final String MAC = "mac";
-	private static final String LINUX = "linux";
-	// app dir stuff
-	private static final String DELIMEAT_DIR = "delimeat";
-	private static final String WINDOWS_DIR = "/Application Data";
-	private static final String MAC_DIR = "/Library/Application Support";
-	// config stuff
-	public static final String CONFIG_FILE_PROPERTY = "io.delimeat.core.config.url";
-	private static final String CONFIG_FILE_URL = "file:APP_DIR/config.xml";
-	// show db stuff
-	public static final String SHOW_DB_PROPERTY = "io.delimeat.core.show.url";
-	private static final String SHOW_DB_URL = "jdbc:derby:APP_DIR/db;create=true";
-
-	public static final String SHOW_DB_DRIVER_PROPERTY = "io.delimeat.core.show.driver";
-	private static final String SHOW_DB_DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
-
-  	private static Logger LOGGER = LoggerFactory.getLogger(ApplicationStartupListener.class);
-
-	private String appPath;
-
-	public ApplicationStartupListener() {
-	}
-
 	@Override
-	public void contextDestroyed(ServletContextEvent arg0) {
-	}
+	public void contextDestroyed(ServletContextEvent arg0) {}
 
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
-		java.util.logging.LogManager.getLogManager().reset();
+		
+		LogManager.getLogManager().reset();
 		SLF4JBridgeHandler.removeHandlersForRootLogger();
 		SLF4JBridgeHandler.install();
-		java.util.logging.Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME).setLevel(java.util.logging.Level.FINEST);
-		
-		LOGGER.debug("Entering contextInitialized");
+		Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).setLevel(Level.FINEST);
 
-		if (System.getProperty(CONFIG_FILE_PROPERTY) == null) {
-			System.setProperty(CONFIG_FILE_PROPERTY, CONFIG_FILE_URL.replace("APP_DIR", getAppPath()));
-		}
-		LOGGER.debug(CONFIG_FILE_PROPERTY + " = " + System.getProperty(CONFIG_FILE_PROPERTY));
-
-		if (System.getProperty(SHOW_DB_PROPERTY) == null) {
-			System.setProperty(SHOW_DB_PROPERTY, SHOW_DB_URL.replace("APP_DIR", getAppPath()));
-		}
-		LOGGER.debug( SHOW_DB_PROPERTY + " = " + System.getProperty(SHOW_DB_PROPERTY));
-
-		if (System.getProperty(SHOW_DB_DRIVER_PROPERTY) == null) {
-			System.setProperty(SHOW_DB_DRIVER_PROPERTY, SHOW_DB_DRIVER);
-		}
-		LOGGER.debug( SHOW_DB_DRIVER_PROPERTY + " = " + System.getProperty(SHOW_DB_DRIVER_PROPERTY));
-		
-		
-		LOGGER.debug("Exiting contextInitialized");
-
-	}
-
-	public String getAppPath() {
-		if (appPath == null) {
-			String userDir = System.getProperty("user.home");
-			String osName = System.getProperty("os.name").toLowerCase();
-			if (osName.startsWith(WINDOWS)) {
-				appPath = userDir + WINDOWS_DIR + "/" + DELIMEAT_DIR;
-			} else if (osName.startsWith(MAC)) {
-				appPath = userDir + MAC_DIR + "/" + DELIMEAT_DIR;
-			} else if (osName.startsWith(LINUX)) {
-				appPath = userDir + "/" + DELIMEAT_DIR;
-			} else {
-				appPath = "";
-			}
-			File dir = new File(appPath);
-			if (!dir.exists()) {
-				// TODO is this actually needed?
-				// dir.mkdirs();
-			}
-		}
-		return appPath;
 	}
 
 }
--- src/test/java/io/delimeat/core/ApplicationStartupListenerTest.java
+++ src/test/java/io/delimeat/core/ApplicationStartupListenerTest.java
@@ -1,8 +1,7 @@
 package io.delimeat.core;
 
-import static org.mockito.Mockito.mock;
-
-import javax.servlet.ServletContextEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -22,57 +21,12 @@ public class ApplicationStartupListenerTest {
 	public void after() {
 		System.clearProperty("user.home");
 		System.clearProperty("os.name");
-		System.clearProperty(ApplicationStartupListener.CONFIG_FILE_PROPERTY);
-		System.clearProperty(ApplicationStartupListener.SHOW_DB_DRIVER_PROPERTY);
-		System.clearProperty(ApplicationStartupListener.SHOW_DB_PROPERTY);
 	}
-
+	
 	@Test
-	public void appPathLinuxTest() {
-		System.setProperty("user.home", "HOME_DIR");
-		System.setProperty("os.name", "LINUX");
-		String result = listener.getAppPath();
-		Assert.assertEquals("HOME_DIR/delimeat", result);
+	public void contextInitializedTest(){
+		listener.contextInitialized(null);
+		Assert.assertEquals(Level.FINEST,Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).getLevel());
 	}
 
-	@Test
-	public void appPathWindowsTest() {
-		System.setProperty("user.home", "HOME_DIR");
-		System.setProperty("os.name", "WINDOWS");
-		String result = listener.getAppPath();
-		Assert.assertEquals("HOME_DIR/Application Data/delimeat", result);
-	}
-
-	@Test
-	public void appPathMacTest() {
-		System.setProperty("user.home", "HOME_DIR");
-		System.setProperty("os.name", "MAC");
-		String result = listener.getAppPath();
-		Assert.assertEquals("HOME_DIR/Library/Application Support/delimeat", result);
-	}
-
-	@Test
-	public void contextInitializedNullValues() {
-		System.setProperty("user.home", "HOME_DIR");
-		System.setProperty("os.name", "LINUX");
-		ServletContextEvent mockedServletContextEvent = mock(ServletContextEvent.class);
-		listener.contextInitialized(mockedServletContextEvent);
-		Assert.assertEquals("file:HOME_DIR/delimeat/config.xml",
-				System.getProperty(ApplicationStartupListener.CONFIG_FILE_PROPERTY));
-		Assert.assertEquals("org.apache.derby.jdbc.EmbeddedDriver",
-				System.getProperty(ApplicationStartupListener.SHOW_DB_DRIVER_PROPERTY));
-		Assert.assertEquals("jdbc:derby:HOME_DIR/delimeat/db;create=true",
-				System.getProperty(ApplicationStartupListener.SHOW_DB_PROPERTY));
-	}
-
-	@Test
-	public void contextInitializedSetValues() {
-		System.setProperty(ApplicationStartupListener.CONFIG_FILE_PROPERTY, "CONFIG_FILE_LOC");
-		System.setProperty(ApplicationStartupListener.SHOW_DB_DRIVER_PROPERTY, "DB_DRIVER");
-		System.setProperty(ApplicationStartupListener.SHOW_DB_PROPERTY, "DB_TYPE");
-
-		Assert.assertEquals("CONFIG_FILE_LOC", System.getProperty(ApplicationStartupListener.CONFIG_FILE_PROPERTY));
-		Assert.assertEquals("DB_DRIVER", System.getProperty(ApplicationStartupListener.SHOW_DB_DRIVER_PROPERTY));
-		Assert.assertEquals("DB_TYPE", System.getProperty(ApplicationStartupListener.SHOW_DB_PROPERTY));
-	}
 }
