286afcade8feb01a94e4346fdf377d35661976e0
admin <admin@delimeat.io>
2016-09-13 02:54:58 +0000

    transform search interval and delay into minutes from ms for display

--- src/main/sourceapp/js/settings/settings_service.js
+++ src/main/sourceapp/js/settings/settings_service.js
@@ -11,8 +11,21 @@ function SettingsService( $resource, ResponseLoggingService, API) {
 	vm._logger = ResponseLoggingService;
 		
 	return $resource(API+'config', {}, {
-    	read: { method: 'GET', interceptor:{responseError: vm._logger.error} },
-        update: { method: 'PUT', interceptor:{responseError: vm._logger.error} }
+    	read: { method: 'GET', interceptor:{responseError: vm._logger.error}, transformResponse: responseTransform  },
+        update: { method: 'PUT', interceptor:{responseError: vm._logger.error}, transformRequest: requestTransform, transformResponse: responseTransform }
     });
+    
+    function responseTransform(data){
+        var newData = angular.fromJson(data);
+        newData.searchDelayMins = Math.round( (newData.searchDelay|0) / 60000 );
+        newData.searchIntervalMins = Math.round( (newData.searchInterval|0) / 60000 );
+        return newData;
+    }
+    
+    function requestTransform(data){
+        data.searchDelay = Math.round( (data.searchDelayMins|0) * 60000 );
+        data.searchInterval = Math.round( (data.searchIntervalMins|0) * 60000 );
+        return angular.toJson(data);        
+    }
     
 }
